dc2ca42434d91798440d924b917b5a922da634e0
Mike O'Connor <mike@mjo4.net>
2024-12-17 17:16:45 -0500

    Initial Commit

---
+++ SeriesTracker/SeriesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeriesTrackerApp.swift
+//  SeriesTracker
+//
+//  Created by Michael O'Connor on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SeriesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
